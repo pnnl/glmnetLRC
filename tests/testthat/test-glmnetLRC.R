ebe1bf4781f6d387c71ec106bf61c5af9755b31f
Landon Sego <landon.sego@pnnl.gov>
2016-03-09 16:47:14 -0700

    Add test of manual fitting with standardization

--- tests/testthat/test-glmnetLRC.R
+++ tests/testthat/test-glmnetLRC.R
@@ -2,7 +2,7 @@ context("Verify glmnetLRC() and associated methods perform correctly")
 
 # Set the seed
 set.seed(20)
-    
+
 # A response variable
 response <- factor(rep(c("a", "b"), each = 20))
 
@@ -34,15 +34,15 @@ test_that("Order of loss matrix specification doesn't make a difference", {
                             lossMat = lossMatrix(c("a", "b", "a", "b"), c("b", "a", "a", "b"), c(1, 1, 0, 0)),
                             alphaVec = alpha, tauVec = 0.5, cvFolds = 3,
                             cvReps = 2, standardize = FALSE, estimateLoss = TRUE)
-   
+
    expect_equal(coef(gfitNewLoss), coef(gfit))
 
    # Get the printed objects
    g1 <- print(gfit, verbose = FALSE)
    g2 <- print(gfitNewLoss, verbose = FALSE)
-   
+
    expect_equal(g1, g2)
-        
+
 })
 
 
@@ -63,25 +63,25 @@ test_that("Final glmnet model matches manual fitting", {
   c2 <- as.vector(coef(gfit1, s = lambda))
 
   # Create an objective function that should match results from glmnet
-  f <- function(beta, n = 40, lambda = 1, alpha = 0.7) {
-  
+  f <- function(beta, n = nrow(preds), lambda = 1, alpha = alpha) {
+
     # Name the regression parameters for easier readability
     beta0 <- beta[1]
     beta1 <- beta[2]
     beta2 <- beta[3]
-  
+
     # The linear combination of regression parameters and predictors
     xb <- beta0 + beta1 * preds[,1] + beta2 * preds[,2]
-  
+
     # The unpenalized binomial log-likelihood
     ll <- sum(y * xb - log(1 + exp(xb))) / n
-  
+
     # The elastic-net penalty
     penalty <- lambda * (0.5 * (1 - alpha) * (beta1^2 + beta2^2) + alpha * (abs(beta1) + abs(beta2)))
-  
+
     # The objective to minimize
     return(penalty - ll)
-      
+
   } # f()
 
   # Fit the parameters manually
@@ -89,13 +89,13 @@ test_that("Final glmnet model matches manual fitting", {
 
   # Tests
   expect_equal(c1, c2)
-  expect_that(c1, not(equals(c3)))  
+  expect_that(c1, not(equals(c3)))
   expect_true(max(abs(c3 - c1)) < 0.001)
 
 })
 
 
-test_that("Testing intercept only model with manual fitting", {
+test_that("Testing non-intercept model with manual fitting", {
 
   # Get a single fit of the data
   gfit <- glmnetLRC(response, preds, alphaVec = alpha, tauVec = 0.5, cvFolds = 3, cvReps = 2,
@@ -121,24 +121,24 @@ test_that("Testing intercept only model with manual fitting", {
   c2 <- c2[-1]
 
   # Create an objective function that should match results from glmnet
-  f <- function(beta, n = 40, lambda = 1, alpha = 0.7) {
-  
+  f <- function(beta, n = nrow(preds), lambda = 1, alpha = alpha) {
+
     # Name the regression parameters for easier readability
     beta1 <- beta[1]
     beta2 <- beta[2]
-  
+
     # The linear combination of regression parameters and predictors
     xb <- beta1 * preds[,1] + beta2 * preds[,2]
-  
+
     # The unpenalized binomial log-likelihood
     ll <- sum(y * xb - log(1 + exp(xb))) / n
-  
+
     # The elastic-net penalty
     penalty <- lambda * (0.5 * (1 - alpha) * (beta1^2 + beta2^2) + alpha * (abs(beta1) + abs(beta2)))
-  
+
     # The objective to minimize
     return(penalty - ll)
-      
+
   } # f()
 
   # Fit the parameters manually
@@ -151,12 +151,75 @@ test_that("Testing intercept only model with manual fitting", {
 
 })
 
+
+test_that("Our understanding of the standardization process is correct", {
+
+  # Get a single fit of the data
+  gfit <- glmnetLRC(response, preds, alphaVec = alpha, tauVec = 0.5, cvFolds = 3, cvReps = 2,
+                    standardize = TRUE)
+
+  # Get optimal parms
+  lambda <- print(gfit, verbose = FALSE)[,"lambda"]
+
+  # Get lambdaVec
+  lambdaVec <- gfit$lambda
+
+  # Fit the glmnet
+  gfit1 <- glmnet::glmnet(preds, response, family = "binomial", alpha = alpha, lambda = lambdaVec,
+                          standardize = TRUE)
+
+  # Get the coefs with the optimal lambda
+  c1 <- coef(gfit)
+  names(c1) <- NULL
+  c2 <- as.vector(coef(gfit1, s = lambda))
+
+  # The standardizing function
+  s <- function(x) sqrt(var(x) * (length(x) - 1) / length(x))
+
+  # Standarize the predictors
+  s1 <- s(preds[,1])
+  s2 <- s(preds[,2])
+
+  # Create an objective function that should match results from glmnet
+  f <- function(beta, n = nrow(preds), lambda = 1, alpha = alpha) {
+
+    # Name the regression parameters for easier readability
+    beta0 <- beta[1]
+    beta1 <- beta[2]
+    beta2 <- beta[3]
+
+    # The linear combination of regression parameters and predictors
+    xb <- beta0 + beta1 * preds[,1] / s1 + beta2 * preds[,2] / s2
+
+    # The unpenalized binomial log-likelihood
+    ll <- sum(y * xb - log(1 + exp(xb))) / n
+
+    # The elastic-net penalty
+    penalty <- lambda * (0.5 * (1 - alpha) * (beta1^2 + beta2^2) + alpha * (abs(beta1) + abs(beta2)))
+
+    # The objective to minimize
+    return(penalty - ll)
+
+  } # f()
+
+  # Fit the parameters manually and backtransform
+  c3 <- optim(c(-3, 1, 1), f, alpha = alpha, n = nrow(preds), lambda = lambda)$par
+  c3[2] <- c3[2] / s1
+  c3[3] <- c3[3] / s2
+
+  # Tests
+  expect_equal(c1, c2)
+  expect_that(c1, not(equals(c3)))
+  expect_true(max(abs(c3 - c1)) < 0.002)
+
+})
+
 test_that("Errors for argument checking are working as expected", {
 
   expect_error(glmnetLRC(rep(1, 2,each = 20), preds, alphaVec = alpha, tauVec = 0.5, cvFolds = 3,
                          cvReps = 1.5, standardize = FALSE, estimateLoss = TRUE),
                "'truthLabels' must be a factor")
-    
+
   expect_error(glmnetLRC(response, preds, alphaVec = alpha, tauVec = 0.5, cvFolds = 3,
                          cvReps = 1.5, standardize = FALSE, estimateLoss = TRUE),
                "'cvReps' must be an integer")
@@ -164,7 +227,7 @@ test_that("Errors for argument checking are working as expected", {
   expect_error(glmnetLRC(response, preds, alphaVec = alpha, tauVec = 0.5, cvFolds = 3,
                          cvReps = 2, stadardize = FALSE, estimateLoss = TRUE),
                "following do not match the arguments in glmnet")
-  
+
   expect_error(glmnetLRC(response, preds, alphaVec = alpha, tauVec = 0.5, cvFolds = 3,
                          cvReps = 2, standardize = FALSE, family = "poisson", estimateLoss = TRUE),
                "and should not be supplied to")
@@ -176,7 +239,7 @@ test_that("Errors for argument checking are working as expected", {
   expect_error(glmnetLRC(response, preds, lossMat = c("a", "b"), alphaVec = alpha,
                          tauVec = 0.5, cvFolds = 3, cvReps = 2),
                "'lossMat' must be either '0-1' or an object of class 'lossMat'")
-  
+
   expect_error(glmnetLRC(response, preds, lossMat = "1-0", alphaVec = alpha,
                          tauVec = 0.5, cvFolds = 3, cvReps = 2),
                "'lossMat' must be either '0-1' or an object of class 'lossMat'")
@@ -220,16 +283,16 @@ test_that("Objects returned by glmnetLRC() remains unchanged", {
   # Test the predict method with a truth col
   np <- predict(gp, testdata, truthCol = "Curated_Quality")
   np.c <- predict(gp.c, testdata, truthCol = "Curated_Quality")
-  
+
   expect_equal(np, np.c)
   expect_equal(summary(np), summary(np.c))
 
-  
+
   # Create another static object for comparison
   gp1 <- glmnetLRC(response, predictors, alphaVec = c(0.5, 0.7),
                    tauVec = c(0.3, 0.7), cvReps = 2,
                    nJobs = 2, masterSeed = 4, stratify = TRUE)
-                   
+
 #  save(gp1, file = "../datasets/gp1.Rdata")
   gp1.c <- Smisc::loadObject("../datasets/gp1.Rdata")
 
@@ -242,10 +305,10 @@ test_that("Objects returned by glmnetLRC() remains unchanged", {
   # Test the predict method with a truth col
   np1 <- predict(gp1, testdata, truthCol = "Curated_Quality")
   np1.c <- predict(gp1.c, testdata, truthCol = "Curated_Quality")
-  
+
   expect_equal(np1, np1.c)
   expect_equal(summary(np1), summary(np1.c))
-  
+
 })
 
 
@@ -259,7 +322,7 @@ test_that("Compare parallel and non-parallel processing", {
                    estimateLoss = TRUE)
 
   gp <- Smisc::loadObject("../datasets/gp.Rdata")
-  
+
   # Test the objects
   expect_equal(gp, gnp)
 
@@ -271,24 +334,24 @@ test_that("Compare parallel and non-parallel processing", {
   # Test the predict method with a truth col
   np <- predict(gp, testdata, truthCol = "Curated_Quality")
   npp <- predict(gnp, testdata, truthCol = "Curated_Quality")
-  
+
   expect_equal(np, npp)
   expect_equal(summary(np), summary(npp))
 
   # Test the predict method without a truth col
   np1 <- predict(gp, testdata, keepCols = 2)
   npp1 <- predict(gnp, testdata, keepCols = "Instrument")
-  
+
   expect_equal(np1, npp1)
   expect_equal(summary(np1), summary(npp1))
-  
+
 })
 
 
 test_that("Test the behavior of the predict method", {
 
   gp <- Smisc::loadObject("../datasets/gp.Rdata")
-    
+
   # Add in a few extra columns for prediction that do not exist in the model
   tmp <- testdata[,25:30]
   colnames(tmp) <- paste(colnames(tmp), "extra", sep = "_")
@@ -327,7 +390,7 @@ test_that("Test the behavior of the predict method", {
   # After accounting for NA's, verify they're equal
   cc <- complete.cases(np5)
   expect_equal(np4[cc,], np5[cc,])
-  
+
 })
 
 test_that("Loss weights perform as expected", {
@@ -352,7 +415,7 @@ test_that("Loss weights perform as expected", {
                    estimateLoss = TRUE)
 
   expect_that(gp1, not(equals(gp.c)))
-    
+
 })
 
 test_that("Seeds make a difference", {
@@ -366,5 +429,5 @@ test_that("Seeds make a difference", {
   gp.c <- Smisc::loadObject("../datasets/gp.Rdata")
 
   expect_that(gp, not(equals(gp.c)))
-    
+
 })
